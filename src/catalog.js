a59774709fc1a9fc40e20da6708c910bcc8309f9
VladimirPopovich27 <117470833+VladimirPopovich27@users.noreply.github.com>
2023-05-10 16:14:17 +0200

    The skeleton of the search block
    Made markup, styles and JS files. Links to command files are disable in the script.

--- src/catalog.js
+++ src/catalog.js
@@ -1,6 +1,6 @@
 import header from './js/header';
 import mobileMenu from './js/mobile-menu';
-
+import seachcatalog from './js/seachcatalog';
 // import weeklyTrends from './js/weekly-trends';
 import modalMovie from './js/modal_movie';
 
