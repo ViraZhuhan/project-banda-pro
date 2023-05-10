9567aa675081e622414806a930f35adaa82440d5
igorekdoroshenko <igorek.doroshenko@gmail.com>
2023-05-10 19:46:07 +0300

    feature pagination-catalog

---
+++ src/js/pagination-catalog.js
@@ -0,0 +1,63 @@
+import Api from './api';
+// import { createWeekTrends } from './createTrends.js';
+// import { createGallery } from './render-card';
+import { pagination } from './pagination';
+
+const api = new Api();
+
+// Установка обработчика события при смене страницы
+pagination.on('beforeMove', async function (eventData) {
+  const currentPage = eventData.page;
+  try {
+    api.setPage(currentPage);
+    const response = await api.weekTrends({ itemsPerPage: 10 });
+    const films = response.results.slice(0, 10);
+    createGallery(films);
+  } catch (error) {
+    console.error('Error fetching movie data:', error);
+  }
+});
+
+// Установка обработчика события при смене страницы поиска
+pagination.on('beforeSearchMove', async function (eventData) {
+  const currentSearchPage = eventData.page;
+  try {
+    api.setSearchPage(currentSearchPage);
+    const response = await api.searchMovieByQuery(api.getSearchQuery(), {
+      itemsPerPage: 10,
+    });
+    const films = response.results.slice(0, 10);
+    createGallery(films);
+  } catch (error) {
+    console.error('Error fetching search results:', error);
+  }
+});
+
+// Инициализация пагинации при загрузке страницы
+async function initializePagination() {
+  try {
+    const response = await api.weekTrends({ itemsPerPage: 10 });
+    const totalItems = response.total_results;
+    pagination.reset(totalItems);
+  } catch (error) {
+    console.error('Error initializing pagination:', error);
+  }
+}
+
+initializePagination();
+
+// Обновление галереи при поиске фильмов
+async function updateGalleryBySearch(query) {
+  try {
+    api.setSearchQuery(query);
+    api.resetSearchPage();
+    const response = await api.searchMovieByQuery(query, { itemsPerPage: 10 });
+    const films = response.results.slice(0, 10);
+    createGallery(films);
+    pagination.resetSearch(response.total_results);
+  } catch (error) {
+    console.error('Error updating gallery by search:', error);
+  }
+}
+
+export { updateGalleryBySearch };
---
+++ src/js/pagination.js
@@ -0,0 +1,28 @@
+import Pagination from 'tui-pagination';
+
+const container = document.getElementById('tui-pagination-container');
+const options = {
+  totalItems: 0,
+  itemsPerPage: 10,
+  visiblePages: 4,
+  page: 1,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}"></span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}"></span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">...</a>',
+  },
+};
+export const pagination = new Pagination(container, options);
