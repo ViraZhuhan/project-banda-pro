3057e9cb383f7d83189d2de970f96ef05c84519c
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2023-05-10 23:13:16 +0300

    Revert "Update catalog.js"

--- src/catalog.js
+++ src/catalog.js
@@ -1,7 +1,7 @@
 import header from './js/header';
 import mobileMenu from './js/mobile-menu';
 import themeToggle from './js/themeToggle';
-import onSearchFormSubmit from './js/seachcatalog';
+import seachcatalog from './js/seachcatalog';
 // import weeklyTrends from './js/weekly-trends';
 import modalMovie from './js/modal_movie';
 
