50e9ebbb7b92474a1319875a9f4a24cc4cf9469d
Vira Zhuhan <115663875+ViraZhuhan@users.noreply.github.com>
2023-05-11 09:13:39 +0300

    підключення герою

--- src/catalog.js
+++ src/catalog.js
@@ -1,8 +1,8 @@
 import header from './js/header';
 import mobileMenu from './js/mobile-menu';
 import themeToggle from './js/themeToggle';
+import './js/hero-home';
 import onSearchFormSubmit from './js/seachcatalog';
 // import weeklyTrends from './js/weekly-trends';
 import modalMovie from './js/modal_movie';
-
 import createWeekTrendsCatalog from './js/create-trends';
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import header from './js/header';
 import mobileMenu from './js/mobile-menu';
 import themeToggle from './js/themeToggle';
+import './js/hero-home';
 // import weeklyTrends from './js/weekly-trends';
 import modalMovie from './js/modal_movie';
 import { fetchUpcomingMovie, renderUpcomingMovieCard } from './js/upcoming';
 import loader from './js/loader';
 import createWeekTrendsCatalog from './js/create-trends';
+
 
