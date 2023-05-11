4f5ce1678aec07dbba5f315cbb81e2d83ef5b364
Vira Zhuhan <115663875+ViraZhuhan@users.noreply.github.com>
2023-05-11 16:55:17 +0300

    add card for hero

---
+++ src/js/hero.js
@@ -0,0 +1,92 @@
+import Api from './api';
+
+import getRefs from './components/get-refs';
+import { genresList } from './components/genre-list';
+import initRating from './init-rating';
+
+
+
+const refs = getRefs();
+
+
+// const root = document.documentElement;
+
+
+// import SubstructBlackDesk from '../images/hero-black-desk.png';
+// import SubstructBlackTab from '../images/hero-black-tab.png';
+// import SubstructWhiteDesk from '../images/hero-white-desk.png';
+// import SubstructWhiteTab from '../images/hero-white-tab.png';
+// import homePageBg from '../images/hero-home-desk.jpg';
+
+
+const api = new Api();
+
+
+getDayMovieTrend();
+
+
+async function getDayMovieTrend() {
+
+randomValue = getRandomHNumber();
+    
+  try {
+    const response = await api.dayTrends();
+      console.log(response.results[getRandomHNumber()]);
+      refs.heroWrapperRef.insertAdjacentHTML('beforeend', renderHeroPageMarkup(response.results[randomValue]));
+
+
+  } catch (err) {
+    // refs.heroWrapperRef.insertAdjacentHTML('beforeend', renderDefaultMarkup());
+  }
+}
+
+
+function getRandomHNumber() {
+  return Math.floor(Math.random() * 10);
+}
+
+
+function renderHeroPageMarkup({
+  id,
+  poster_path,
+  title,
+  overview,
+  vote_average,
+}) {
+    const imageUrl = poster_path
+        ? `https://image.tmdb.org/t/p/w1280/${poster_path}`
+        : 'https://via.placeholder.com/395x574?text=No+Image';
+
+      return `
+      <h1 class="hero__title">${title}</h1>
+      <img class="hero__movie-descripton" src="${imageUrl}" alt="${title}" width="395" >
+      <div class="rating hero__vote">
+        <div class="rating__body">
+          <div class="rating__active" style="width: ${
+            vote_average * 10
+          }%;"></div>
+          <div class="rating__items hero__vote">
+            <input type="radio" class="rating__item" value="1" name="rating" />
+            <input type="radio" class="rating__item" value="2" name="rating" />
+            <input type="radio" class="rating__item" value="3" name="rating" />
+            <input type="radio" class="rating__item" value="4" name="rating" />
+            <input type="radio" class="rating__item" value="5" name="rating" />
+          </div>
+        </div>
+        <div class="rating__value">${vote_average}</div>
+      </div>
+      <p class="hero__text hero__movie-descripton" movie-id="${id}>${overview}</p>
+      <button class="hero__btn">Watch trailer</button>
+     `;
+}
+
+
+function renderDefaultMarkup() {
+  return `
+    <h1 class="hero__title">Let’s Make Your Own Cinema</h1>
+  <p class="hero__text">Is a guide to creating a personalized movie theater experience.
+   You'll need a projector, screen, and speakers.<span class="paragraph__hidden">Decorate your space,
+   choose your films, and stock up on snacks for the full experience.</span></p>
+   <a href="/src/catalog.html" class="hero__btn">Get Started</a>
+   `;
+}
